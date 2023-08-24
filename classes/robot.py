0f09e9d20db12fc422e320ec74b1f3633216e197
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 09:55:14 -0400

    create class robot

--- classes/robot.py
+++ classes/robot.py
@@ -1,2 +1,12 @@
 class Robot:
-    pass
+    def __init__ (self, name, age, model_num, battery_type="A"):
+        self.name = name
+        self.age = 0
+        self.model_num = model_num
+        self.battery_type = battery_type
+
+    def eat_corn(self):
+        return "Delicious"
+
+    def swim(self):
+        return f"Hey someone need help {self.name} the robot is going to save you"
