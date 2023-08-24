188a00ad063ddb64008db6114135c1af0338de03
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 09:56:51 -0400

    provide example

--- index.py
+++ index.py
@@ -4,4 +4,7 @@ from classes.human import Human
 
 print('Beep boop')
 
-robot1 = Robot("greg", 265, 1870)
+robot1 = Robot("greg", 265, 1870, "AAA")
+person1 = Human("Norman", "Orteaga", 27)
+
+import ipdb; ipdb.set_trace()
