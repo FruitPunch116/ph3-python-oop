b9ae04a46e1286b4a8115005aede467fdc3bbf50
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-24 09:54:43 -0400

    creaate class human

--- classes/human.py
+++ classes/human.py
@@ -1,2 +1,24 @@
 class Human:
-    pass
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self._age = 27
+
+    def __repr__(self):
+        return f"Person (name= {self.first_name}, last name= {self.last_name}, age= {self._age})"
+    
+    def say_full_name(self):
+        return f"Thank you for calling Verizon my name is {self.first_name} {self.last_name}, how can I help you?"
+
+    def happy_birthday(self):
+        self._age +=1
+        return f"Today I became {self._age}!"
+
+    @property
+    def age(self):
+        return f"{self.first_name} is {self._age} don't tell anybody"
+    
+    @age.setter
+    def age(self, new_age):
+        if new_age < self._age or new_age > self._age :
+            print("I know your address, blood type, skin color and age. Do yourself a favor and quit lying about your age.")
